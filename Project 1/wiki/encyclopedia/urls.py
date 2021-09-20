9ba0858adb621b6ab6006e3ae11d36546e3d6a13
GCFernandes <guilherme.correa.fernandes@usp.br>
2021-09-20 12:50:20 -0300

    add page edditor and random page link

--- Project 1/wiki/encyclopedia/urls.py
+++ Project 1/wiki/encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<slug:slug>", views.entries, name = "entries"),
-    path("search", views.search, name = "search")
+    path("search", views.search, name = "search"),
+    path("editor/<slug:slug>", views.editor, name = "editor"),
+    path("new_page", views.new_page, name = "new_page"),
+    path("random_page", views.random_page, name = "random_page")
 ]
--- Project 1/wiki/encyclopedia/views.py
+++ Project 1/wiki/encyclopedia/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from . import util
 from markdown2 import Markdown
 from django.http import HttpResponseNotFound
+from random import randrange
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -16,14 +17,15 @@ def entries(request, slug):
     if(entry != None):
         entry = markdowner.convert(entry)
         return render(request, "encyclopedia/entries.html", {
-            "entry": entry
+            "entry": entry,
+            "page_name": slug
         })
     else:
         return HttpResponseNotFound('<h1>Error 404: Page not found</h1>')
 
 def search(request):
     query = request.GET.get('q')
-    
+
     if query in util.list_entries():
         return(entries(request, query))
 
@@ -31,3 +33,28 @@ def search(request):
     return render(request, "encyclopedia/search.html", {
         "entries": results
     })
+
+def editor(request, slug):
+    if(request.method == 'POST'):
+        content = request.POST.get("entry_content")
+        title = content.split('\n')[0]
+        title = title.replace("#", "")
+        title = title.replace(" ", "")
+        title = title.replace("\r", "")
+
+        util.save_entry(title, content)
+
+    entry = util.get_entry(slug)
+    if(entry == None):
+        entry = ""
+    return render(request, "encyclopedia/editor.html", {
+        "entry": entry
+    })
+
+def new_page(request):
+    return editor(request, None)
+
+def random_page(request):
+    all_entries = util.list_entries()
+    selected = all_entries[randrange(len(all_entries))]
+    return(entries(request, selected))
